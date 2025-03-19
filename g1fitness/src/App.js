fdfa9d1fecd90a62102c22bff746eacf45677130
mysterydoggy <47798667+mysterydoggy@users.noreply.github.com>
2025-03-19 22:59:54 +0000

    beta version of g1

--- g1fitness/src/App.js
+++ g1fitness/src/App.js
@@ -1,14 +1,42 @@
-import logo from './logo.svg';
+import logo from './G1_Logo.png';
+import dogVideo from "./Wenomechainsama.mp4";
 import './App.css';
+
+
+
+
+
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="logo-container">
+          <div className="button-group">
+            <button className="nav-button">Workout Library</button>
+            <button className="nav-button">Create a workout</button>
+            <button className="nav-button">Journal</button>
+            <button className="nav-button">About us</button>
+            <button className="nav-button">Sign in</button>
+
+          </div>
+        </div>
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
+
          Hey guys
         </p>
+
+        <h1>Welcome to G1 Fitness</h1>
+
+        <video width="600" controls autoPlay loop muted className="spinning-video">
+          <source src={dogVideo}  type="video/mp4" />
+        </video>
         <a
           className="App-link"
           href="https://reactjs.org"
